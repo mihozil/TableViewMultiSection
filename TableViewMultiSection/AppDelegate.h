f5010cf761d8b0398a9bae6943d7729b831c5e39
Apple <Apple@Apples-MacBook-Air.local>
2016-01-09 14:26:51 +0700

    Initial Commit

---
+++ TableViewMultiSection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewMultiSection
+//
+//  Created by Apple on 1/9/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewMultiSection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewMultiSection
+//
+//  Created by Apple on 1/9/16.
+//  Copyright (c) 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
